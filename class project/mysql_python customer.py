8939afc466dce725e7f61e13874eb05f1c121f14
Naren_karthick <narenkarthickgururaju@gmail.com>
2024-06-16 13:16:45 +0530

    updated file

--- class project/mysql_python customer.py
+++ class project/mysql_python customer.py
@@ -14,7 +14,7 @@ cur = con.cursor()
 
 
 def create_customer():
-    q = "create table customer(cid int primary key , cname varchar(30), balance float, dob data)"
+    q = "create table customer(cid int primary key , cname varchar(30), balance float, dob date)"
     cur.execute(q)
     con.commit()
     print("Created succesful")
@@ -35,33 +35,70 @@ def select():
     q = "select * from customer"
     cur.execute(q)
     data = cur.fetchall()
-    print(tabulate(data,headers=['Cid','Cname','Balance','Dob']))
+    print(tabulate(data, headers=["Cid", "Cname", "Balance", "Dob"]))
+
 
 def create_transaction():
-    q='create table transaction(tid int autoincrement ,cid int,tdate date ,amt float, ttype varchar(10))'
+    q = "create table transaction(tid int auto_increment primary key ,cid int,tdate date ,amt float, ttype varchar(10))"
     cur.execute(q)
     con.commit()
-    print('created succesful')
-
+    print("created succesful")
 
 
 def insert_transaction():
-    pass
+    cid = int(input("Enter Customer Id :"))
+    tdate = input("Enter the transaction data:")
+    amt = input("Enter transaction amount:")
+    ttype = input("Enter transaction type(d,w):")
+    q = 'insert into transaction(cid,tdate,amt,ttype) values({},"{}",{},"{}")'.format(
+        cid, tdate, amt, ttype
+    )
+    cur.execute(q)
+    con.commit()
+    print("Inserted succesful")
+
+
 def update():
-    q='select c.cid, c.balance,t.amt,t.ttype from customer c , transaction t where c.cid=t.cid'
+    q = "select c.cid, c.balance,t.amt,t.ttype from customer c , transaction t where c.cid=t.cid"
     cur.execute(q)
     data = cur.fetchall()
     for i in data:
-        if i[3].lower() == 'd':
-            nbal = i[1]+i[2]
-            q='update customer set balance= {} where cid={}'.format(nbal,i[0])
+        if i[3].lower() == "d":
+            nbal = i[1] + i[2]
+            q = "update customer set balance= {} where cid={}".format(nbal, i[0])
             cur.execute(q)
             con.commit()
-        elif i[3].lower() == 'w':
-            nbal = i[1]-i[2]
-            q='update customer set balance= {} where cid={}'.format(nbal,i[0])
+        elif i[3].lower() == "w":
+            nbal = i[1] - i[2]
+            q = "update customer set balance= {} where cid={}".format(nbal, i[0])
             cur.execute(q)
             con.commit()
-        
-        
+        print(i[0], nbal)
+    print("Updated succesfully")
 
+
+while True:
+    print("1.Create table-customer ")
+    print("2.create table-transaction")
+    print("3.Insert-customer")
+    print("4.insert-transaction")
+    print("5.update ")
+    print("6. To display")
+    print("10.Exit")
+    ch = int(input("Enter your choice:"))
+    if ch == 1:
+        create_customer()
+    elif ch == 2:
+        create_transaction()
+    elif ch == 3:
+        insert_customer()
+    elif ch == 4:
+        insert_transaction()
+    elif ch == 5:
+        update()
+    elif ch == 6:
+        select()
+    elif ch == 10:
+        break
+    else:
+        print("Invalid choice")
